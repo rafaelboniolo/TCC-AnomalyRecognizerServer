d13b85b454d20f8466ab370d9a61e47927124664
Rafael Boniolo <33812322+rafaelboniolo@users.noreply.github.com>
2019-11-28 20:13:20 -0300

    remove import

--- src/main/java/com/tcc/recognize/Main.java
+++ src/main/java/com/tcc/recognize/Main.java
@@ -6,7 +6,6 @@ import com.tcc.recognize.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
